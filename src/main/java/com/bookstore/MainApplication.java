2cd253c4d0e7a5c5847c87439bb3f7e2b5bb264f
SridharChallabotla <sridhar.challabotla@metlife.com>
2020-02-03 22:08:27 +0530

    first commit

---
+++ src/main/java/com/bookstore/MainApplication.java
@@ -0,0 +1,48 @@
+package com.bookstore;
+
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApplication {
+  
+    private final BookstoreService bookstoreService;
+
+    public MainApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }        
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {            
+            bookstoreService.fetchAuthorWithBooksById(4, "History");
+        };
+    }
+}
+
+/*
+ * 
+ * 
+ * How To Map An Entity To a Query (@Subselect) in a Spring Boot Application
+
+Note: Consider using @Subselect only if using DTO, DTO and extra queries, or map a database view to an entity is not a solution.
+
+Description: This application is an example of mapping an entity to a query via Hibernate, @Subselect. Mainly, we have two entities in a bidirectional one-to-many association. An Author has wrote several Book. The idea is to write a read-only query to fetch from Author only some fields (e.g., DTO), but to have the posibility to call getBooks() and fetch the Book in a lazy manner as well. As you know, a classic DTO cannot be used, since such DTO is not managed and we cannot navigate the associations (don’t support any managed associations to other entities). Via Hibernate @Subselect we can map a read-only and immutable entity to a query. This time, we can lazy navigate the associations.
+
+Key points:
+
+define a new entity that contains only the needed fields from the Author (including association to Book)
+for these fields, define only getters
+mark the entity as @Immutable since no write operations are allowed
+flush pending state transitions for the used entities by @Synchronize
+use @Subselect to write the needed query, map an entity to an SQL query
+ * 
+ * 
+ */
